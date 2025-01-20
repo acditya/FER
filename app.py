aac9afc24e776620da46e05cc6edeb6619ee6d08
acditya <achditya@gmail.com>
2025-01-20 17:42:10 +0400

    Test

--- app.py
+++ app.py
@@ -51,6 +51,7 @@ def upload_file():
         return jsonify({'emotion': emotion})
 
 if __name__ == '__main__':
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True)
+    # if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    #     os.makedirs(app.config['UPLOAD_FOLDER'])
+    # app.run(debug=True)
+    print(predict_emotion("Adi_DP.jpg"))
